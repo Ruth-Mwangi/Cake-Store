ee4114862cd6bc88792c69789c13af18d2736a52
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-03-13 12:34:25 +0300

    Updated all files

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,30 @@
+
 $(document).ready(function(){
+
+
+    // active class navigation
+
+    $(".navbar .nav a").click(function(){
+        
+        $(".navbar .nav").find('a.active').removeClass( 'active' ).css({"background-color":'',"color":""});
+        
+        $(this).addClass('active').css({"background-color":"cornsilk","color":"black"});
+        
+        
+
+    })
+
+
+
+
     $(".card").mouseenter(function(){
         
-        $(".card-img-overlay").fadeToggle('2000',"swing");
+
+        $(this).children(".card-img-overlay").fadeToggle("fast");
+        
     });
     $(".card").mouseleave(function () { 
-        $(".card-img-overlay").fadeToggle("slow");
+        $(this).children(".card-img-overlay").fadeToggle("slow");
         
     });
 
@@ -15,14 +35,31 @@ $(document).ready(function(){
         
         switch (btnValue){
             case ("allbtn"):
+                $(".card").show();
                 break;
             case ("cakesbtn"):
+                $(".card.cakes").show();
+                $(".card.sweets").hide();
+                $(".card.donuts").hide();
+                $(".card.cupcakes").hide();
                 break;
             case ("sweetsbtn"):
+                $(".card.sweets").show();
+                $(".card.cakes").hide();
+                $(".card.donuts").hide();
+                $(".card.cupcakes").hide();
                 break;
             case ("cupcakesbtn"):
+                $(".card.cupcakes").show();
+                $(".card.sweets").hide();
+                $(".card.cakes").hide();
+                $(".card.donuts").hide();
                 break;
             case ("donutsbtn"):
+                $(".card.donuts").show();
+                $(".card.cakes").hide();
+                $(".card.sweets").hide();
+                $(".card.cupcakes").hide();
                 break;
             
         }
